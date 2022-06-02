de2792d043ff115127087653a35598ff44645eb0
leon-augusto <leonaugusto087@gmail.com>
2022-06-02 12:13:27 -0300

    Aula 09 - Relacionando entidades utilizando _OneToMany e _ManyToOne

---
+++ src/main/java/com/eventosapp/models/Participante.java
@@ -0,0 +1,40 @@
+package com.eventosapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Participante {
+
+    @Id
+    private String matricula;
+    private String nomeParticipante;
+
+    @ManyToOne
+    private Evento evento;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNomeParticipante() {
+        return nomeParticipante;
+    }
+
+    public void setNomeParticipante(String nomeParticipante) {
+        this.nomeParticipante = nomeParticipante;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+}
